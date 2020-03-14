687f6efd78d06afab96f10ad021797ab0ffa09a5
bandanishanth <bandanishanth@gmail.com>
2020-03-14 15:25:53 +0530

    Added Remaining POST Requests as well as forms
    Signed-off-by: bandanishanth <bandanishanth@gmail.com>

--- app.py
+++ app.py
@@ -20,13 +20,37 @@ def hello():
 
 @app.route("/")
 def index():
-    return "Index Page"
+    return render_template('index.html', my_style="/static/styles/form_style.css")
 
 
-@app.route('/form')
-def form():
-    return render_template('form.html', my_style="/static/styles/form_style.css",
-                           my_script="/static/scripts/form_validate.js")
+@app.route("/knn_form")
+def knn_form():
+    return render_template('knn_form.html', my_style="/static/styles/form_style.css")
+
+
+@app.route("/gaussian_form")
+def gaussain_form():
+    return render_template('gaussian_form.html', my_style="/static/styles/form_style.css")
+
+
+@app.route("/logreg_form")
+def logreg_form():
+    return render_template('logreg_form.html', my_style="/static/styles/form_style.css")
+
+
+@app.route("/rf_form")
+def rf_form():
+    return render_template('rf_form.html', my_style="/static/styles/form_style.css")
+
+
+@app.route("/perceptron_form")
+def perceptron_form():
+    return render_template('perceptron_form.html', my_style="/static/styles/form_style.css")
+
+
+@app.route("/sgd_form")
+def sgd_form():
+    return render_template('sgd_form.html', my_style="/static/styles/form_style.css")
 
 
 @app.route("/gaussian_predict", methods=['POST'])
@@ -45,6 +69,54 @@ def gaussian_predict():
 def knn_predict():
     data = request.form.to_dict()
     prediction = gaussian_model.predict(pd.DataFrame.from_dict([data]))
+    print(prediction)
+    output = prediction[0]
+    if output == 0:
+        return render_template('result.html', prediction="No")
+    else:
+        return render_template('result.html', prediction="Yes")
+
+
+@app.route("/logreg_predict", methods=['POST'])
+def logreg_predict():
+    data = request.form.to_dict()
+    prediction = logreg_model.predict(pd.DataFrame.from_dict([data]))
+    print(prediction)
+    output = prediction[0]
+    if output == 0:
+        return render_template('result.html', prediction="No")
+    else:
+        return render_template('result.html', prediction="Yes")
+
+
+@app.route("/perceptron_predict", methods=['POST'])
+def perceptron_predict():
+    data = request.form.to_dict()
+    prediction = perceptron_model.predict(pd.DataFrame.from_dict([data]))
+    print(prediction)
+    output = prediction[0]
+    if output == 0:
+        return render_template('result.html', prediction="No")
+    else:
+        return render_template('result.html', prediction="Yes")
+
+
+@app.route("/rf_predict", methods=['POST'])
+def rf_predict():
+    data = request.form.to_dict()
+    prediction = rf_model.predict(pd.DataFrame.from_dict([data]))
+    print(prediction)
+    output = prediction[0]
+    if output == 0:
+        return render_template('result.html', prediction="No")
+    else:
+        return render_template('result.html', prediction="Yes")
+
+
+@app.route("/sgd_predict", methods=['POST'])
+def sgd_predict():
+    data = request.form.to_dict()
+    prediction = sgd_model.predict(pd.DataFrame.from_dict([data]))
     print(prediction)
     output = prediction[0]
     if output == 0:
